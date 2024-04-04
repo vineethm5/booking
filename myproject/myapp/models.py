3fe0854f42f2d54ba8db31d5d79897825457b974
vineethm5 <160957@sdmcujire.in>
2024-04-04 19:19:14 +0530

    added templates

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class booking(models.Model):
+    hname=models.TextField(max_length=1000)
+    hprice=models.TextField(max_length=100)
+    description=models.TextField(max_length=100)
+    entered_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
